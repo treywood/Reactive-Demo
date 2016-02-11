2c9e10764450ea468201904cc76ae7889a4f3533
Trey Wood <edwinjwood3rd@gmail.com>
2016-02-11 16:46:07 -0600

    remove API_KEY from source control

--- src/js/services/translate.js
+++ src/js/services/translate.js
@@ -1,17 +1,16 @@
 import Http from './http';
-
-const API_KEY = "AIzaSyDA_YOxrZbHJoV-iGmaND6nCJQrSIUQ5j0";
+import Env from '../env';
 
 export default class TranslateService {
 
   static translateText(from, to, text) {
     text = encodeURIComponent(text);
-    return Http.get(`https://www.googleapis.com/language/translate/v2?key=${API_KEY}&source=${from}&target=${to}&q=${text}`)
+    return Http.get(`https://www.googleapis.com/language/translate/v2?key=${Env.API_KEY}&source=${from}&target=${to}&q=${text}`)
       .then(response => _.get(response, "data.translations[0].translatedText", null));
   }
 
   static getLanguages() {
-    return Http.get(`https://www.googleapis.com/language/translate/v2/languages?key=${API_KEY}&target=en`)
+    return Http.get(`https://www.googleapis.com/language/translate/v2/languages?key=${Env.API_KEY}&target=en`)
       .then(response => {
         let languages = _.get(response, "data.languages", []);
         return _.sampleSize(languages, 10);
