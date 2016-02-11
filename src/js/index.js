6d48aec45bdf9b9c9318826d50c705028c63239c
Trey Wood <edwinjwood3rd@gmail.com>
2016-02-11 17:47:28 -0600

    reorg

--- src/js/index.js
+++ src/js/index.js
@@ -7,28 +7,32 @@ let submitButton = $("#submitButton");
 let inputText = $("#englishInput");
 let outputText = $("#output");
 
-let availableLanguagesStream =
-  Rx.Observable.fromEvent(refreshButton, "click").startWith("")
-  .flatMap(() => TranslateService.getLanguages());
 
-let languagesSubscriber = availableLanguagesStream.subscribe(languages => {
-  let choices = languages.map(lang => $(`<option value="${lang.language}">${lang.name}</option>`));
-  languageOptions.html(choices);
-  languageOptions.prepend(`<option value=""></option>`);
-});
-
-// UI Streams
+// Input Value Streams
 let targetLanguageStream = Rx.Observable.fromEvent(languageOptions, "change").pluck("target", "value");
 let inputTextStream = Rx.Observable.fromEvent(inputText, "keydown").pluck("target", "value").distinctUntilChanged();
 
+// Trigger Streams
 let buttonClickStream = Rx.Observable.fromEvent(submitButton, "click");
 let enterKeyStream = Rx.Observable.fromEvent(inputText, "keydown").filter(e => e.keyCode === 13);
+
+// Data Streams
+let availableLanguagesStream =
+  Rx.Observable.fromEvent(refreshButton, "click").startWith("")
+  .flatMap(() => TranslateService.getLanguages());
 
 let translationStream =
   Rx.Observable.merge(buttonClickStream, enterKeyStream)
   .withLatestFrom(targetLanguageStream, (e, lang) => lang)
   .withLatestFrom(inputTextStream)
   .flatMap(([language, input]) => TranslateService.translateText('en', language, input));
+
+// Subscribers
+let languagesSubscriber = availableLanguagesStream.subscribe(languages => {
+  let choices = languages.map(lang => $(`<option value="${lang.language}">${lang.name}</option>`));
+  languageOptions.html(choices);
+  languageOptions.prepend(`<option value=""></option>`);
+});
 
 let translationSubscriber = translationStream.subscribe(translatedText => {
   outputText.html(translatedText);
