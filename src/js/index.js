3bb0e27c5b27d2896a2307e765fdc35515f0c36b
Trey Wood <edwinjwood3rd@gmail.com>
2016-02-17 14:57:53 -0600

    some loading indication

--- src/js/index.js
+++ src/js/index.js
@@ -17,22 +17,32 @@ let enterKeyStream = Rx.Observable.fromEvent(inputText, "keydown").filter(e => e
 let refreshButtonClickStream = Rx.Observable.fromEvent(refreshButton, "click");
 
 // Data Streams
-let availableLanguagesStream = refreshButtonClickStream.startWith("")
-  .flatMap(() => TranslateService.getLanguages());
+let availableLanguagesStream = refreshButtonClickStream.startWith(void 0)
+  .flatMap(() => TranslateService.getLanguages().delay(1000));
 
 let translationStream = Rx.Observable.merge(submitButtonClickStream, enterKeyStream)
   .withLatestFrom(targetLanguageStream, _.nthArg(1))
   .withLatestFrom(inputTextStream)
-  .flatMap(([language, input]) => TranslateService.translateText('en', language, input));
+  .flatMap(([language, input]) => TranslateService.translateText('en', language, input).delay(1000));
 
 // Subscribers
-let languagesSubscriber = availableLanguagesStream.subscribe(languages => {
+let languagesLoadingSubscriber = refreshButtonClickStream.startWith(void 0).subscribe(() => {
+  languageOptions.html(`<option value="">Loading...</option>`);
+  languageOptions.prop('disabled', true);
+});
+
+let languagesLoadedSubscriber = availableLanguagesStream.subscribe(languages => {
   let choices = languages.map(lang => $(`<option value="${lang.language}">${lang.name}</option>`));
   languageOptions.html(choices);
   languageOptions.prepend(`<option value=""></option>`);
+  languageOptions.prop('disabled', false);
 });
 
-let translationSubscriber = translationStream.subscribe(translatedText => {
+let translatingSubscriber = Rx.Observable.merge(submitButtonClickStream, enterKeyStream).subscribe(() => {
+  outputText.html(`<strong>Translating...</strong>`);
+});
+
+let translatedSubscriber = translationStream.subscribe(translatedText => {
   outputText.html(translatedText);
   inputText.val("");
 });
